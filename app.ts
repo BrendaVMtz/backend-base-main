5132a0bb19651e096b6dd89c866ee30fe0d0020e
Sarai <sarai.ilopez@roatech.com.mx>
2024-05-09 10:53:12 -0600

    cambios revision

--- app.ts
+++ app.ts
@@ -1,8 +1,36 @@
-import dotnenv from "dotenv";
+/* import dotnenv from "dotenv";
 import { Server } from './models/server';
 
 dotnenv.config();
 
 const server = new Server();
 
-server.listen();
+server.listen(); */
+
+
+import express from "express";
+import cors from "cors";
+import usersRouter from "./routes/usuarios.routers";
+import dbConnection from "./database/connect";
+
+const app = express();
+
+app.use(express.json())  // la comunicacion entre servidor y cliente es con JSON
+app.use(cors()) // las referencias cruzadas
+
+const PORT = 3000;
+
+(async () => {
+    try {
+        await dbConnection.authenticate(); // conexion a la Base de datos
+        console.log("Database connected successfully");
+    } catch (error) {
+        console.error("Unable to connect to the database:", error);
+    }
+
+    app.use('/api', usersRouter); // el enrutamiento que van a tener tus peticiones
+
+    app.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}`);
+    });
+})();
--- database/connect.ts
+++ database/connect.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize";
 const db = new Sequelize('proyecto', 'root', 's4r4i2024#', {
     
     host: "localhost",
-    port:  3306,
+    //port:  3306,
     // database: "proyecto",
     // username: "root",
     // password: "s4r4i2024#",
